19fe621e5163131d18aef0cbe818514c00ac5890
evansthitai <evansnt9619@gmail.com>
2023-10-05 23:18:57 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide =  () => {let num1 = 2000
+let num2 = 100
+const total = (num1 / num2) 
+return total}
+divide ()
+
+const square = (x) => x ** x
+square(2)
+
+const add = (a , b) => a + b 
+add (3,4)
+
